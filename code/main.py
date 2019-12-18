d757df76aafd21833f2d89218ced4626933cfb41
Tobias Dorra <public.tobias@outlook.com>
2019-12-18 10:49:54 +0100

    read dataset

--- code/main.py
+++ code/main.py
@@ -27,43 +27,15 @@ def show_image_from_file(path):
 def load_image(filename):
     print(filename)
     i = nibabel.load(filename)
-    print(i.affine)
     return i.get_data(), i.affine[0, 0], i.affine[1, 1], i.affine[2, 2]
 
-
-def make_image_volumetric(image: np.ndarray, h):
-    # project the 3d image onto a 2d plane with rgb colors
-    # using the emission-absorpion model.
-    # and show the result
-
-    # normalize values to be between 0 and 1
-    max = image.max()
-    min = image.min()
-    image = (image - min) / (max - min)
-
-    # create image, filled with some grey bg colour
-    size_x, size_y, size_z = image.shape
-    image_r = np.ones((size_x, size_y)) * .5
-    image_g = np.ones((size_x, size_y)) * .5
-    image_b = np.ones((size_x, size_y)) * .5
-
-    # apply the layers from back to front
-    for z in range(size_z):
-        layer = image[:, :, z]
-        dz = h / size_z
-        absorption = np.exp(-(0.3 * layer) * dz)
-        emission_r = .4 * layer * dz
-        emission_g = 4 * layer ** 5 * dz
-        emission_b = .2 * (1-layer) ** 5 * dz
-        image_r = image_r * absorption + emission_r
-        image_g = image_g * absorption + emission_g
-        image_b = image_b * absorption + emission_b
-
-    # show result
-    return np.stack([image_r, image_g, image_b], axis=2)
+def load_segmentation_mask(filename):
+    i = nibabel.load(filename)
+    print(i.get_data().shape)
+    print(repr(i))
 
 
-def make_image_slices(image, nr_slices):
+def make_image_slices(image, nr_slices, colours=True):
 
     # normalize values to be between 0 and 1
     img_max = image.max()
@@ -79,15 +51,19 @@ def make_image_slices(image, nr_slices):
 
     # concatenate and show
     imagestrip = np.concatenate(slices, axis=1)
-    imagestrip_rgb = np.stack([
-        np.maximum(np.minimum(imagestrip * 2 - .5, 1), 0),
-        np.maximum(imagestrip * 2 - 1, 0),
-        np.maximum(1 - imagestrip * 2, 0)
-    ], axis=2)
+    if colours:
+        imagestrip_rgb = np.stack([
+            np.maximum(np.minimum(imagestrip * 2 - .5, 1), 0),
+            np.maximum(imagestrip * 2 - 1, 0),
+            np.maximum(1 - imagestrip * 2, 0)
+        ], axis=2)
+    else:
+        imagestrip_rgb = np.stack([imagestrip] * 3, axis=2)
+
     return imagestrip_rgb
 
 
-def show_image_collage(image, nr_slices, sx, sy, sz):
+def show_image_collage(image, nr_slices, sx, sy, sz, labels=None):
 
     # front, side and top view
     view1 = image
@@ -95,23 +71,44 @@ def show_image_collage(image, nr_slices, sx, sy, sz):
     view3 = np.moveaxis(view2, 2, 0)
     views = [view1, view2, view3]
     aspects = [sy / sx, sz / sx, sy / sz]
-    sizes = [sz, sy, sx]
+    slice_views = [make_image_slices(v, nr_slices, labels is None) for v in views]
 
-    volume_views = [make_image_volumetric(v, s) for v, s in zip(views, sizes)]
-    slice_views = [make_image_slices(v, nr_slices) for v in views]
-    complete_views = [np.concatenate([v, s], axis=1) for v, s in zip(volume_views, slice_views)]
+    if labels is not None:
+        label_view1 = labels
+        label_view2 = np.moveaxis(label_view1, 2, 0)
+        label_view3 = np.moveaxis(label_view2, 2, 0)
+        label_views = [label_view1, label_view2, label_view3]
+        label_slice_views = [make_image_slices(v, nr_slices) for v in label_views]
+        slice_views = [0.5*a + 0.5*b for a, b in zip(slice_views, label_slice_views)]
 
     for i in range(3):
         plt.subplot(3, 1, i + 1)
-        plt.imshow(complete_views[i], aspect=aspects[i])
+        plt.imshow(slice_views[i], aspect=aspects[i])
     plt.show()
 
+
+def load_dataset(base_path):
+    images_tr_path = os.path.join(base_path, "imagesTr")
+    images_ts_path = os.path.join(base_path, "imagesTs")
+    labels_tr_path = os.path.join(base_path, "labelsTr")
+    images_tr = {f: os.path.join(images_tr_path, f) for f in os.listdir(images_tr_path) if f.endswith(".nii.gz") and not f.startswith(".")}
+    labels_tr = {f: os.path.join(labels_tr_path, f) for f in os.listdir(labels_tr_path) if f.endswith(".nii.gz") and not f.startswith(".")}
+    images_ts = {f: os.path.join(images_ts_path, f) for f in os.listdir(images_ts_path) if f.endswith(".nii.gz") and not f.startswith(".")}
+    tr = {f: (images_tr[f], labels_tr[f]) for f in images_tr if f in labels_tr}
+    return tr, images_ts
 
 # Daten liegen im data Ordner, der sich im roo des Projektes befindet.
 def main():
     print("Gude!")
-    show_image_from_file(LIVER_PATH + TRAINING + 'liver_0.nii.gz')
+
     image, sx, sy, sz = load_image(LIVER_PATH + TRAINING + 'liver_0.nii.gz')
+    segmentation, _, _, _ = load_image(LIVER_PATH + LABELS + 'liver_0.nii.gz')
+    show_image_collage(image, 5, sx, sy, sz, segmentation)
+
+    image, sx, sy, sz = load_image(LIVER_PATH + TEST + 'liver_132.nii.gz')
     show_image_collage(image, 5, sx, sy, sz)
+
+    all_files = load_dataset(LIVER_PATH)
+    print(repr(all_files))
 
 main()
